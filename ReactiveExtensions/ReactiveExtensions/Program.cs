453e18785743a61694f761bc5bea58456a2156dd
Hangjit Rai <hangjit.rai@ofx.com>
2018-10-31 15:39:33 +1100

    Initial Commit

---
+++ ReactiveExtensions/ReactiveExtensions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ReactiveExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!", Thread.CurrentThread.ManagedThreadId);
+            var query = Enumerable.Range(1, 5).Select(num => num);
+
+            foreach (var number in query)
+            {
+                Console.WriteLine(number); 
+            }
+
+            ImDone();
+        }
+
+        private static void ImDone()
+        {
+            Console.WriteLine("I'm done!");
+        }
+    }
+}
